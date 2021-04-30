eca3cc1bab88aa428c8dd0bc839d54c09e9cbdb7
Zvi <75641016+RunningDeer@users.noreply.github.com>
2021-04-30 11:44:28 +0300

    Add files via upload
    Assignment 1

---
+++ HelloWorldModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class HelloWorldModel
+    {
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string HomeTown { get; set; }
+        public string PathToImage { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult HelloWorld()
+        {
+            HelloWorldModel model = new HelloWorldModel
+            {
+                Name = "Zvi Rotsztein",
+                DOB = Convert.ToDateTime("1987 - 01 - 01"),
+                HomeTown="London",
+                PathToImage= "https://beithorah.co.il/wp-content/uploads/2020/06/%D7%94%D7%A8%D7%91-%D7%A6%D7%91%D7%99-%D7%A8%D7%95%D7%98%D7%A0%D7%A9%D7%98%D7%99%D7%99%D7%9F-1.jpg"
+            };
+            return View(model);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
